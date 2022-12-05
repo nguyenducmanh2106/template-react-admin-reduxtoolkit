4b521703e56a2cb91798ad1c8d36866fd789b23b
IIGVNAM\Manh.ND <nguyenducmanh2106@gmail.com>
2022-12-05 13:34:16 +0700

    init project

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080/",
+        changeOrigin: true,
+      },
+      "/o/": {
+        target: "http://localhost:8080/",
+        changeOrigin: true,
+      },
+      "/h/": {
+        target: "http://localhost:8080/",
+        changeOrigin: true,
+      },
+      "^/u/\\d*/rss.xml": {
+        target: "http://localhost:8080/",
+        changeOrigin: true,
+      },
+    },
+  },
+});
